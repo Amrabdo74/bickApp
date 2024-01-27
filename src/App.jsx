889834fef65fd14d8c7c808fa8a17a154cf89b3d
Amrabdo74 <andkpe12@gmail.com>
2024-01-27 16:32:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Headerdata from "./components/Headerdata";
+
+const App = () => {
+  return <>
+  <Header />
+  <Headerdata />
+  
+  </>;
+};
+
+export default App;
